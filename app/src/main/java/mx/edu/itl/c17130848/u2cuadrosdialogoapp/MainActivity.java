69d1e10853160a960ec18c240bb791b04b5e6c6d
Angel Eduardo Soto <angel.14.98@hotmail.com>
2021-03-16 12:54:13 -0600

    Terminado

--- app/src/main/java/mx/edu/itl/c17130848/u2cuadrosdialogoapp/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c17130848/u2cuadrosdialogoapp/MainActivity.java
@@ -1,3 +1,36 @@
+/*------------------------------------------------------------------------------------------
+:*                         TECNOLOGICO NACIONAL DE MEXICO
+:*                       INSTITUTO TECNOLOGICO DE LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                             DESARROLLO EN ANDROID "A"
+:*
+:*                   SEMESTRE: ENE-JUN/2021    HORA: 10-11 HRS
+:*
+:*          Clase que despliega distintos cuadros de mensaje y dialogo basicos
+:*
+:*  Archivo     : MainActivity.java
+:*  Autor       : Angel Eduardo Soto García     17130848
+:*  Fecha       : 16/Mar/2021
+:*  Compilador  : Android Studio 4.1.2
+:*  Descripción :
+:*      Los diálogos de alerta a presentar serán los siguientes:
+:*            •	Toast de duracion corta
+:*            •	Toast de duracion larga
+:*            •	Mensaje Snack de duración corta
+:*            •	Cuadro de dialogo simple
+:*            •	Cuadro de dialogo con botón OK
+:*            •	Cuadro de dialogo con botón OK y Cancelar
+:*            •	Cuadro de dialogo con lista de opciones
+:*            •	Cuadro de dialogo con lista de opciones selección única
+:*            •	Cuadro de dialogo con lista de opciones selección multiple
+:*            •	Cuadro de dialogo con un diseño de usuario y contraseña incrustado
+:*            •	Cuadro de dialogo Acerca de
+:*
+:*  Ultima modif:
+:*  Fecha       Modificion             Motivo
+:*==========================================================================================
+:*
+:*------------------------------------------------------------------------------------------*/
 package mx.edu.itl.c17130848.u2cuadrosdialogoapp;
 
 import androidx.appcompat.app.AlertDialog;
@@ -6,10 +39,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -122,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-                        mostrarToast("Nuevo color favorito: " + colores [ which ]);
+                        mostrarToast("Nuevo color favorito: " + colores [ iColorFavorito ]);
                     }
                 })
                 .create()
@@ -130,21 +166,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //----------------------------------------------------------------------------------------------
-
+    //Cuadro de dialogo con lista de opciones con casillas de verificacion para selección multiple
+    private  boolean coloresSeleccionados [] = { false, false, false }; //Ninguno aparecera seleccionada
+    private ArrayList<String> coloresFavoritos = new ArrayList<>();
     public void btnDialogoListaSeleccionMultipleClick ( View v ) {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        builder.setTitle( "Seleccione sus colores favoritos" )
+                .setMultiChoiceItems(colores, coloresSeleccionados, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if ( isChecked ){
+                            mostrarToast( "Color seleccionado: " + colores [ which ] );
+                            coloresFavoritos.add(colores[which].toString());
+                        }
+                        else {
+                            mostrarToast( "Color deseleccionado: " + colores [which] );
+                            coloresFavoritos.remove(colores[which].toString());
+                        }
+                    }
+                })
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarToast( "Colores favoritos: "+ coloresFavoritos);
+                    }
+                })
+                .create()
+                .show();
     }
 
     //----------------------------------------------------------------------------------------------
-
+    //Ciadro de dialogo con un layout de login incrustado y botones Aceptar y Cancelar
+    private View login_layout;
+    private EditText edtUsuario;
+    private EditText edtContrasena;
     public void btnDialogoLayoutIncrustadoClick ( View v ) {
+        //Obtener la instancia del layout de login y de sus campos Usuario y Contraseña
+        login_layout = getLayoutInflater().inflate( R.layout.login_layout, null );
+        edtUsuario = login_layout.findViewById( R.id.edtNombre );
+        edtContrasena = login_layout.findViewById( R.id.edtContrasena );
 
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        builder.setTitle( "Acceso" )
+                .setIcon( R.drawable.itl )
+                .setView( login_layout )
+                .setPositiveButton("Entrar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarToast( "Bienvendio: " +
+                                edtUsuario.getText().toString() + " (" +
+                                edtContrasena.getText().toString() + ")"
+                        );
+                    }
+                })
+                .setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); // cerrar el dialogo sin hacer nada
+                    }
+                })
+                .setCancelable( false )
+                .create()
+                .show();
     }
 
     //----------------------------------------------------------------------------------------------
-
+    // Cuadro de dialogo que muestra información del creador
     public void btnAcercaDeClick ( View v ) {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Acerca de")
+                .setIcon( R.drawable.itl )
+                .setMessage( "CuadroDialogoApp v1.0.\n\n" +
+                             "TecNM Campus La Laguna.\n\n" +
+                             "Angel Eduardo Soto García (17130848).\n\n" +
+                             "(C) Derechos reservados 2021. Torreón Coah. México.")
+                .create()
+                .show();
     }
 
     //----------------------------------------------------------------------------------------------
